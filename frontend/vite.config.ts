06a65ea192c7b46cdb8faf4ed2c46e6186f5a5d0
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2026-06-09 21:34:11 -0300

    Teste

---
+++ frontend/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Circuito Verde Teresópolis',
+        short_name: 'CVT',
+        description: 'Meu aplicativo PWA',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
