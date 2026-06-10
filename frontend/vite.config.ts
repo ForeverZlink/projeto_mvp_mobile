d11a406a45e6259ec4605f4fabc7c4e1b22d4669
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2026-06-09 22:03:40 -0300

    Novo pwa corrigido

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,12 +17,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/logo.png',
+            src: '/logo_192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/logo.png',
+            src: '/logo_512.png',
             sizes: '512x512',
             type: 'image/png',
           },
